957830a88939cb9048803135b69248f5d165016c
jtx007 <jamesjacobthomas7@gmail.com>
2018-07-22 15:20:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 // Code your solution in this file!
-const logDriverNames = function  (drivers) {
+const logDriverNames = function (drivers) {
   drivers.forEach(function (driver) {
     console.log(driver.name);
   })
 }
 
-const logDriversByHometown = function (drivers, location) {
-    drivers.forEach(function (driver) {
-      if (driver.hometown === location) {
-        console.log(driver.name)
-      }
-    })
-  }
+const logDriversByHometown = function (drivers, hometown) {
+  let arr =  drivers.filter( function (driver) {
+    return driver.hometown === hometown
+  })
+  logDriverNames(arr)
 
-  const driversByRevenue = function (drivers) {
-    const revUp = drivers.slice()
-    revUp.sort(function (a, b) {
-      return a.revenue - b.revenue
-    })
-    return revUp
-  }
+}
 
-  const driversByName = function (drivers) {
-    const names = drivers.slice()
-     names.sort(function (a, b) {
-      return a.name.localeCompare(b.name)
-    })
-    return names
-  }
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driver1, driver2) {
+    return driver1.revenue - driver2.revenue
+  })
+}
 
-  const totalRevenue = function (drivers) {
-      return drivers.reduce(function (total, driver) {
-        return driver.revenue + total
-      }, 0)
-    }
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driver1, driver2) {
+    return driver1.name.localeCompare(driver2.name)
+  })
+}
 
-    const averageRevenue = function (drivers) {
-      return totalRevenue(drivers) / drivers.length
-    }
+const totalRevenue = function (drivers) {
+  return drivers.slice().reduce(function (acc, driver) {
+    return driver.revenue + acc
+  }, 0)
+}
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length
+}
